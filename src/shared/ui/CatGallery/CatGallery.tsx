94a850b9c6b4ff1659258b20823405c5ea5d35f0
Leont1cov <chuchunovvakov@mail.ru>
2026-04-06 21:20:57 +0300

    feat: add empty state for favorites and conditional loader display

--- src/shared/ui/CatGallery/CatGallery.tsx
+++ src/shared/ui/CatGallery/CatGallery.tsx
@@ -7,18 +7,29 @@ interface CatListProps {
     isLoading: boolean;
     observerRef: React.RefObject<HTMLDivElement | null>;
     children: ReactNode;
+    activeTab: 'all' | 'fav';
 }
 
-export const CatList = ({ isLoading, observerRef, children }: CatListProps) => {
+export const CatList = ({ cats, isLoading, observerRef, children, activeTab }: CatListProps) => {
+    if (activeTab === 'fav' && cats.length === 0) {
+        return (
+            <main className={styles.main}>
+                <div className={styles.loader}>Здесь пока ничего нет</div>
+            </main>
+        );
+    }
+
     return (
         <main className={styles.main}>
             <div className={styles.grid}>
                 {children}
             </div>
 
-            <div ref={observerRef} className={styles.loader}>
-                {isLoading ? '... загружаем еще котиков ...' : 'Листай ниже'}
-            </div>
+            {activeTab === 'all' && (
+                <div ref={observerRef} className={styles.loader}>
+                    {isLoading ? '... загружаем еще котиков ...' : 'Листай ниже'}
+                </div>
+            )}
         </main>
     );
 };
