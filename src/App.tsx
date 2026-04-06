b9997d9f31d80fd260c43bdfecf1586e22d5aa2b
Leont1cov <chuchunovvakov@mail.ru>
2026-04-06 17:17:43 +0300

    feat: integrate CatAPI and display images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import type {Cat} from "./shared/types/cat.ts";
+import {fetchCats} from "./shared/api/api.ts";
 
 function App() {
+    const [cats, setCats] = useState<Cat[]>([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetchCats()
+            .then(setCats)
+            .catch(console.error)
+            .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return <div>Loading...</div>
+
   return (
-    <>
-        <h1>Hello VK</h1>
-    </>
+      <main style={{ padding: '20px' }}>
+          <div style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fill, minmax(225px, 1fr))',
+              gap: '20px'
+          }}>
+              {cats.map(cat => (
+                  <img
+                      key={cat.id}
+                      src={cat.url}
+                      alt="cat"
+                      style={{ width: '100%', height: '225px', objectFit: 'cover' }}
+                  />
+              ))}
+          </div>
+      </main>
   )
 }
 
---
+++ src/shared/api/api.ts
@@ -0,0 +1,16 @@
+import type {Cat} from "../types/cat.ts";
+
+const API_KEY = import.meta.env.VITE_API_KEY
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+export const fetchCats = async (limit: number = 10): Promise<Cat[]> => {
+    const response = await fetch(`${BASE_URL}/images/search?limit=${limit}`, {
+        headers: {
+            'x-api-key': API_KEY
+        }
+    })
+
+    if (!response.ok) throw new Error('Ошибка при загрузки котиков')
+
+    return response.json()
+}
